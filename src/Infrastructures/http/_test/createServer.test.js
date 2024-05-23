067129c109de027e41cf8cf79249766bad1aebd1
RivalDartmedia <123264773+RivalDartmedia@users.noreply.github.com>
2024-05-23 14:54:02 +0700

    add say hello world test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -2,6 +2,8 @@
 const pool = require('../../database/postgres/pool');
 const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
 const container = require('../../container');
+const AuthenticationsTableTestHelper = require('../../../../tests/AuthenticationsTableTestHelper');
+// const injections = require('../../injections');
 const createServer = require('../createServer');
  
 describe('HTTP server', () => {
@@ -11,6 +13,7 @@ describe('HTTP server', () => {
  
   afterEach(async () => {
     await UsersTableTestHelper.cleanTable();
+    await AuthenticationsTableTestHelper.cleanTable();
   });
  
   it('should response 404 when request unregistered route', async () => {
@@ -23,6 +26,22 @@ describe('HTTP server', () => {
     });
     // Assert
     expect(response.statusCode).toEqual(404);
+  });
+
+  describe('when GET /', () => {
+    it('should return 200 and hello world', async () => {
+      // Arrange
+      const server = await createServer({});
+      // Action
+      const response = await server.inject({
+        method: 'GET',
+        url: '/',
+      });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.value).toEqual('Hello world!');
+    });
   });
  
   describe('when POST /users', () => {
